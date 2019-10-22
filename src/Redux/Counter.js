80b5ca9b3300236c9ce6086d2a1bfaebbe149522
Your Name <Desi Suci Ramadhani>
2019-10-22 12:13:20 +0700

    Desi Suci Ramadhani

---
+++ src/Redux/Counter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { increment, decrement } from './action/index';
+
+class Counter extends Component {
+    addIncrement = () => {
+        console.log(this.props);
+        this.props.incrementAction();
+    };
+
+    minIncrement = () => {
+        console.log(this.props);
+        this.props.decrementAction();
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>Contoh Redux</Text>
+                <Text>Counter : {this.props.count} </Text>
+                <Button title="Tambah" onPress={this.addIncrement} />
+                <Button title="Kurang" onPress={this.minIncrement} />
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    //console.log(state); //lihat data reducer
+    return {
+        count: state.count,
+    };
+}
+
+function matchDispatchToProps(dispatch) { //convert dari action menjadi props
+    //fungsi bindActionCreators : mengkombinasikan semua action ke single object
+    return bindActionCreators(
+        { incrementAction: increment, decrementAction: decrement },
+        dispatch,
+    );
+}
+export default connect(
+    mapStateToProps,
+    matchDispatchToProps,
+)(Counter);
